220e0c7b5a88f5920de6272fe3a19182fddc9710
pannaga-rj <153255080+pannaga-rj@users.noreply.github.com>
2025-05-16 17:31:22 +0530

    Update todo.py

--- Lab01/todo.py
+++ Lab01/todo.py
@@ -1,3 +1,27 @@
+from sklearn.datasets import load_iris
+import pandas as pd
+
+iris = load_iris()
+df = pd.DataFrame(iris.data, columns=iris.feature_names)
+df.head()
+
+df['target'] = iris.target
+
+df
+
+
+
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("abdulmalik1518/mobiles-dataset-2025")
+
+print("Path to dataset files:", path)
+
+df = pd.read_csv("/content/Mobiles_Dataset_(2025).csv", encoding='latin-1') # or 'ISO-8859-1', or 'cp1252'
+df.head()
+df['Company Name']
+
 data = {"USN" : ['1', "2", "3"], "Name" : ["A", "B", "C"]}
 df = pd.DataFrame(data)
 df
@@ -11,4 +35,60 @@ df.head()
 
 
 
+df.columns
 
+
+df = pd.read_csv("/content/Dataset_of_Diabetes .csv")
+df.head()
+
+
+import yfinance as yf
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+tickers = ["RELIANCE.NS", "TCS.NS", "INFY.NS"]
+# Fetch historical data for the last 1 year
+
+data = yf.download(tickers, start="2022-10-01", end="2023-10-01", group_by='ticker')
+
+# Display the first 5 rows of the dataset
+
+print("First 5 rows of the dataset:")
+
+print(data.head())
+
+print("\nShape of the dataset:")
+
+print(data.shape)
+
+
+
+# Summary statistics for a specific stock (e.g., Reliance)
+
+reliance_data = data['RELIANCE.NS']
+
+print("\nSummary statistics for Reliance Industries:")
+
+print(reliance_data.describe())
+
+# Calculate daily returns
+
+reliance_data['Daily Return'] = reliance_data['Close'].pct_change()
+
+
+# Plot the closing price and daily returns
+
+plt.figure(figsize=(12, 6))
+
+plt.subplot(2, 1, 1)
+
+reliance_data['Close'].plot(title="Reliance Industries - Closing Price")
+
+plt.subplot(2, 1, 2)
+
+reliance_data['Daily Return'].plot(title="Reliance Industries - Daily Returns", color='orange')
+
+plt.tight_layout()
+
+plt.show()
